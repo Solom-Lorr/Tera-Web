4d8510141639a46f6a82373e80633408e87f223b
Jordan <jordan.rouco-miguez@epitech.eu>
2022-02-03 19:42:31 +0100

    reset repo

--- launcher/js/login.js
+++ launcher/js/login.js
@@ -1,50 +1,55 @@
-﻿$(function () {
-    var date = new Date();
-    var m = 60 * 24 * 7;	//7 day
-    date.setTime(date.getTime() + (m * 60 * 1000));
+﻿
+	$(function() {
+		var date = new Date();
+ 		var m = 60*24*7;	//7 day
+ 		date.setTime(date.getTime() + (m * 60 * 1000));
 
-    if (ACCOUNT_NAME != '' && ACCOUNT_KEY != '') {
-        parent.LoginSuccess();
-    }
+		if(ACCOUNT_NAME != '' && ACCOUNT_KEY != '' ){ //&& !isNaN(ACCOUNT_NAME)
+			parent.LoginSuccess();	
+		}//else $('#loginScreen').show();
+		
+		var idSave = $.cookie('idSave');
+		var idValue = $.cookie('idValue');
+		$('#userID').val(idValue);
+		if(idValue != '' && idValue != null){			
+			$('#checkboxIDSave').attr("checked",true);
+		}
+		
+		$("#checkboxIDSave").click(function(){
+			if($(this).attr("checked") == 'checked')
+			{
+				$.cookie('idSave', true, { expires: date });
+				$.cookie('idValue', $('#userID').val(), { expires: date });				
+			}
+			else
+			{
+				$.cookie('idSave', false, { expires: date });
+				$.cookie('idValue', null, { expires: date });			
+				
+			}
+		});
+		
+	  });
 
-    var idSave = $.cookie('idSave');
-    var idValue = $.cookie('idValue');
-    $('#userID').val(idValue);
-    if (idValue != '' && idValue != null) {
-        $('#checkboxIDSave').attr("checked", true);
-    }
-
-    $("#checkboxIDSave").click(function () {
-        if ($(this).attr("checked") == 'checked') {
-            $.cookie('idSave', true, {expires: date});
-            $.cookie('idValue', $('#userID').val(), {expires: date});
-        } else {
-            $.cookie('idSave', false, {expires: date});
-            $.cookie('idValue', null, {expires: date});
-
-        }
-    });
-
-});
-
-function idRegCheck(id) {
-    var id_regx = /^[0-9a-zA-Z]{4,13}$/;
-
-    if (!id_regx.test(id.value)) {
-        alert('ID has to be betweeen 4 and 13 characters');
-        id.value = "";
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function pwRegCheck(pw) {
-    if (pw.value.length < 8 || pw.value.length > 14) {
-        alert('Password has to be between 8 and 14  characters');
-        pw.value = "";
-        return false;
-    } else {
-        return true;
-    }
-}
+	function idRegCheck(id){
+		var id_regx=/^[0-9a-zA-Z]{4,13}$/;
+		
+		if(!id_regx.test(id.value)){
+			alert('帳號請輸入4-13個英數字元');
+			id.value="";
+			//id.focus();
+			return false;
+		}else{
+			return true;
+		}
+	}
+	function pwRegCheck(pw){
+		if(pw.value.length < 8 || pw.value.length >14){
+			alert('密碼請輸入8-14個英數字元');
+			pw.value="";
+			//pw.focus();
+			return false;
+		}else{
+			return true;
+		}
+	}	
